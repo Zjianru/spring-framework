b0a2433a272976a0ae39a933d9505f6193dcf20b
zjianru <864156480@qq.com>
2024-03-20 01:28:08 +0800

    add main class

---
+++ spring-cz-test/src/main/java/com/cz/demo/Main.java
@@ -0,0 +1,15 @@
+package com.cz.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * code desc
+ *
+ * @author Zjianru
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
